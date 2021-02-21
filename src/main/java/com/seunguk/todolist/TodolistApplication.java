459d0f14abb87b268e4d4e8324a6add868faa8df
seunguk <seunguk329@gmail.com>
2021-02-21 21:09:50 +0900

    그래들수정

--- src/main/java/com/seunguk/todolist/TodolistApplication.java
+++ src/main/java/com/seunguk/todolist/TodolistApplication.java
@@ -2,6 +2,7 @@ package com.seunguk.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 어노테이션들을 모두 활성화할 수 있도록 Application 클래스에 활성화 어노테이션을 추가합니다.
